e575d99b5eb1d1d1febe02ef221ee4cbcf887a31
Nicolas Araujo <nicolasaraujo.sj@gmail.com>
2021-09-01 14:33:10 -0300

    create presentation package and move main activity to it

--- app/src/main/java/com/example/gameapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/gameapp/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.gameapp
+package com.example.gameapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.gameapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
