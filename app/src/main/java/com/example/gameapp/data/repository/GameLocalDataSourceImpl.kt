da0b2224d7ea0a492212c36659953c433852710a
Nicolas Araujo <nicolasaraujo.sj@gmail.com>
2021-09-01 12:42:45 -0300

    create class that implements local data source interface

---
+++ app/src/main/java/com/example/gameapp/data/repository/GameLocalDataSourceImpl.kt
@@ -0,0 +1,21 @@
+package com.example.gameapp.data.repository
+
+import com.example.gameapp.data.db.GameDao
+import com.example.gameapp.data.model.game.Game
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class GameLocalDataSourceImpl(private val gameDao: GameDao) : GameLocalDataSource {
+    override suspend fun getGamesFromDatabase(): List<Game> = gameDao.getGames()
+
+    override suspend fun insertGameToDatabase(game: List<Game>) {
+        CoroutineScope(Dispatchers.IO).launch { gameDao.insertGames(game) }
+    }
+
+    override suspend fun deleteAllGamesFromDatabase() {
+        CoroutineScope(Dispatchers.IO).launch {
+            gameDao.deleAllGames()
+        }
+    }
+}
