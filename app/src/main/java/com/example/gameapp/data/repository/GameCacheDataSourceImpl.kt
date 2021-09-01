a2341d109d93482f5abff2460554213f80270d32
Nicolas Araujo <nicolasaraujo.sj@gmail.com>
2021-09-01 12:43:28 -0300

    create class that implements cache data source interface

---
+++ app/src/main/java/com/example/gameapp/data/repository/GameCacheDataSourceImpl.kt
@@ -0,0 +1,15 @@
+package com.example.gameapp.data.repository
+
+import com.example.gameapp.data.model.game.Game
+
+class GameCacheDataSourceImpl : GameCacheDataSource {
+
+    private var gameList = ArrayList<Game>()
+
+    override suspend fun getGamesFromCache(): List<Game> = gameList
+
+    override suspend fun saveGamesToCache(game: List<Game>) {
+        gameList.clear()
+        gameList = ArrayList(game)
+    }
+}
