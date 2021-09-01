d3dbb66e19732ceb5da8313ac3cd061b6aef0c4f
Nicolas Araujo <nicolasaraujo.sj@gmail.com>
2021-09-01 16:59:36 -0300

    di test

--- app/src/main/java/com/example/gameapp/presentation/GameActivity.kt
+++ app/src/main/java/com/example/gameapp/presentation/GameActivity.kt
@@ -2,11 +2,27 @@ package com.example.gameapp.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.ViewModelProvider
 import com.example.gameapp.R
+import com.example.gameapp.presentation.di.Injector
+import javax.inject.Inject
+
+private const val TAG = "GameActivity"
 
 class GameActivity : AppCompatActivity() {
+
+    @Inject
+    lateinit var factory: GameViewModelFactory
+    private lateinit var gameViewModel: GameViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+        (application as Injector).createGameSubComponent().inject(this)
+        gameViewModel = ViewModelProvider(this, factory).get(GameViewModel::class.java)
+        gameViewModel.getGames().observe(this, {
+            Log.d(TAG, "$it")
+        })
     }
 }
