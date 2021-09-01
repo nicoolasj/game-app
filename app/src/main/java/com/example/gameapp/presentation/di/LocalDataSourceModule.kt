1550a6a082e9a242e7506c2cbf83aa5e712257f4
Nicolas Araujo <nicolasaraujo.sj@gmail.com>
2021-09-01 15:45:41 -0300

    dagger: create local data source module

---
+++ app/src/main/java/com/example/gameapp/presentation/di/LocalDataSourceModule.kt
@@ -0,0 +1,18 @@
+package com.example.gameapp.presentation.di
+
+import com.example.gameapp.data.db.GameDao
+import com.example.gameapp.data.repository.game.datasource.GameLocalDataSource
+import com.example.gameapp.data.repository.game.datasourceImpl.GameLocalDataSourceImpl
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+class LocalDataSourceModule {
+
+    @Singleton
+    @Provides
+    fun provideGameLocalDataSource(gameDao: GameDao): GameLocalDataSource {
+        return GameLocalDataSourceImpl(gameDao)
+    }
+}
