d31e6dedfc07d0649f77743aa9c920d93931ef3b
Nicolas Araujo <nicolasaraujo.sj@gmail.com>
2021-09-01 11:02:38 -0300

    add room entity and primary key annotations for game dataclass

--- app/src/main/java/com/example/gameapp/data/model/game/Game.kt
+++ app/src/main/java/com/example/gameapp/data/model/game/Game.kt
@@ -1,11 +1,15 @@
 package com.example.gameapp.data.model.game
 
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import com.google.gson.annotations.SerializedName
 
+@Entity(tableName = "game_table")
 data class Game(
     @SerializedName("background_image")
     val backgroundImage: String,
+    @PrimaryKey
     @SerializedName("id")
     val id: Int,
     @SerializedName("metacritic")
