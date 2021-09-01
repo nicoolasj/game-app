d361dab0faa5a31e7b9c8555fecd86f6fcad6ef8
Nicolas Araujo <nicolasaraujo.sj@gmail.com>
2021-09-01 10:57:55 -0300

    create game package and move corresponding game dataclasses to it

--- app/src/main/java/com/example/gameapp/data/api/RawgService.kt
+++ app/src/main/java/com/example/gameapp/data/api/RawgService.kt
@@ -1,6 +1,6 @@
 package com.example.gameapp.data.api
 
-import com.example.gameapp.data.model.GameList
+import com.example.gameapp.data.model.game.GameList
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
--- app/src/main/java/com/example/gameapp/data/model/game/GameList.kt
+++ app/src/main/java/com/example/gameapp/data/model/game/GameList.kt
@@ -1,4 +1,4 @@
-package com.example.gameapp.data.model
+package com.example.gameapp.data.model.game
 
 
 import com.google.gson.annotations.SerializedName
