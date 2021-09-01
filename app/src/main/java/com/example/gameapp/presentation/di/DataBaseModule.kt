c878297b2b52757b9ef523266ac3bc7007cd9763
Nicolas Araujo <nicolasaraujo.sj@gmail.com>
2021-09-01 15:34:38 -0300

    dagger: create database module for room database and game dao

---
+++ app/src/main/java/com/example/gameapp/presentation/di/DataBaseModule.kt
@@ -0,0 +1,27 @@
+package com.example.gameapp.presentation.di
+
+import android.content.Context
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.gameapp.data.db.GameDao
+import com.example.gameapp.data.db.RawgDatabase
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+class DataBaseModule {
+
+    @Singleton
+    @Provides
+    fun provideGameDatabase(context: Context): RawgDatabase {
+        return Room.databaseBuilder(context, RawgDatabase::class.java, "game_database")
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideGameDao(rawgDatabase: RawgDatabase): GameDao {
+        return rawgDatabase.gameDao()
+    }
+}
