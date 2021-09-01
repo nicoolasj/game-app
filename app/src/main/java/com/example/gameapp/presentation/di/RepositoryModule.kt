9fdc50145779495a558cf57b35073229304f6435
Nicolas Araujo <nicolasaraujo.sj@gmail.com>
2021-09-01 15:54:24 -0300

    dagger: create repository module

---
+++ app/src/main/java/com/example/gameapp/presentation/di/RepositoryModule.kt
@@ -0,0 +1,24 @@
+package com.example.gameapp.presentation.di
+
+import com.example.gameapp.data.repository.game.GameRepositoryImpl
+import com.example.gameapp.data.repository.game.datasource.GameCacheDataSource
+import com.example.gameapp.data.repository.game.datasource.GameLocalDataSource
+import com.example.gameapp.data.repository.game.datasource.GameRemoteDataSource
+import com.example.gameapp.domain.repository.GameRepository
+import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
+
+@Module
+class RepositoryModule {
+
+    @Singleton
+    @Provides
+    fun provideGameRepository(
+        gameRemoteDataSource: GameRemoteDataSource,
+        gameLocalDataSource: GameLocalDataSource,
+        gameCacheDataSource: GameCacheDataSource
+    ): GameRepository {
+        return GameRepositoryImpl(gameRemoteDataSource, gameLocalDataSource, gameCacheDataSource)
+    }
+}
