1e474dd4ff5acf42361bf0de4e4226d2efa0d7b7
Nicolas Araujo <nicolasaraujo.sj@gmail.com>
2021-09-01 10:53:02 -0300

    create service interface for api

---
+++ app/src/main/java/com/example/gameapp/data/api/RawgService.kt
@@ -0,0 +1,12 @@
+package com.example.gameapp.data.api
+
+import com.example.gameapp.data.GameList
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface RawgService {
+
+    @GET("games")
+    suspend fun getGames(@Query("key") key: String): Response<GameList>
+}
